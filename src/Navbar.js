935323a78d3daf3bdbed5120345ef0986dc21ba7
akhila <akhilatadi@gmail.com>
2022-04-07 12:46:00 -0400

    initial commit

---
+++ src/Navbar.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import "./Navbar.css"
+
+function Navbar() {
+const [show, handleShow]= useState()
+
+useEffect(() => {
+window.addEventListener("scroll", () =>{
+    if(window.scrollY > 100){
+    handleShow(true);
+    } else handleShow(false);
+});
+return () => {
+    window.removeEventListener("scroll");
+};
+},[]);
+
+    return (
+        <div className={
+            `nav ${show && "nav_black"}`} >
+            <img className='nav_logo'
+            
+                src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"
+           alt="Netflix Logo"
+            />
+           <img className='nav_avatar'
+            
+            src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+       alt="Avatar Logo"
+        />
+        </div>
+    )
+}
+
+export default Navbar
+
+
+//https://assets.brand.microsites.netflix.io/assets/7dc497e2-4975-11ec-a9ce-066b49664af6_cm_1440w.jpg?v=1
